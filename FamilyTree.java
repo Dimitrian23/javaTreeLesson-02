aac9c1ea660f9e552ed2113207782d6f2dfc4125
Dmitrian23 <dimonio1991@mail.ru>
2024-06-05 20:22:10 +0300

    familytree

---
+++ FamilyTree.java
@@ -0,0 +1,43 @@
+﻿import java.util.ArrayList;
+import java.util.List;
+
+public class FamilyTree {
+    private List<Human> familyMembers;
+
+    public FamilyTree() {
+        this.familyMembers = new ArrayList<>();
+    }
+
+    public void addMember(Human human) {
+        familyMembers.add(human);
+    }
+
+    public Human findMember(String name) {
+        for (Human human : familyMembers) {
+            if (human.getName().equals(name)) {
+                return human;
+            }
+        }
+        return null;
+    }
+
+    public void printFamilyTree() {
+        for (Human human : familyMembers) {
+            System.out.println(human);
+        }
+    }
+
+    public List<Human> getFamilyMembers() {
+        return familyMembers;
+    }
+
+    public void save(String filename) {
+        FileHandler handler = new FileHandler();
+        handler.save(this, filename);
+    }
+
+    public static FamilyTree load(String filename) {
+        FileHandler handler = new FileHandler();
+        return handler.load(filename);
+    }
+}
