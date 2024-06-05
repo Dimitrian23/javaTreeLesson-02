cea9a4b0b16ac805c1344a66628390966c31436d
Dmitrian23 <dimonio1991@mail.ru>
2024-06-05 20:24:01 +0300

    human

---
+++ Human.java
@@ -0,0 +1,84 @@
+﻿import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+public class Human implements Serializable {
+
+    private String name;
+    private LocalDate dateOfBirth;
+    private LocalDate dateOfDeath;
+    private List<Human> children;
+    private Human father;
+    private Human mother;
+    private Gender gender;
+
+    public Human(String name, Gender gender, LocalDate dateOfBirth, LocalDate dateOfDeath) {
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.dateOfDeath = dateOfDeath;
+        this.gender = gender;
+        this.children = new ArrayList<>();
+    }
+
+    public Human(String name, Gender gender, LocalDate dateOfBirth) {
+       this(name,  gender, dateOfBirth,null);
+    }
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public LocalDate getDateOfDeath() {
+        return dateOfDeath;
+    }
+
+    public List<Human> getChildren() {
+        return children;
+    }
+
+    public Human getFather() {
+        return father;
+    }
+
+    public Human getMother() {
+        return mother;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setDateOfDeath(LocalDate dateOfDeath) {
+        this.dateOfDeath = dateOfDeath;
+    }
+
+    public void addChild(Human child) {
+        children.add(child);
+    }
+
+    public void setFather(Human father) {
+        this.father = father;
+    }
+
+    public void setMother(Human mother) {
+        this.mother = mother;
+    }
+
+    @Override
+    public String toString() {
+        return "Человек{" +
+                "имя='" + name + '\'' +
+                ", дата рождения=" + dateOfBirth +
+                ", дата смерти=" + (dateOfDeath != null ? dateOfDeath : "Alive") +
+                ", пол=" + gender +
+                '}';
+    }
+
+
+}
